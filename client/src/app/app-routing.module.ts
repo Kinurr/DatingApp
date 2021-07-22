03456f82493b75da5339a60a55b82e5d4d4ea991
Kinurr <josemfigueiredo@outlook.pt>
2021-07-22 17:25:58 +0100

    Added routing and more components.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { HomeComponent } from './home/home.component';
 
-
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "members", component: MemberListComponent},
+  {path: "members/:id", component: MemberDetailComponent},
+  {path: "lists", component: ListsComponent},
+  {path: "messages", component: MessagesComponent},
+  {path: "**", component: HomeComponent, pathMatch: "full"},
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
